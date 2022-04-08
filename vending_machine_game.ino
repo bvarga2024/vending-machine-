4e6d8d3f14d7a2b359f11dcecd630336faf415b5
bvarga2024 <93211792+bvarga2024@users.noreply.github.com>
2022-04-08 11:55:24 -0400

    Add files via upload

---
+++ vending_machine_game.ino
@@ -0,0 +1,54 @@
+int buttonA = 2;
+int buttonB = 3;
+int greenled = 4;
+int redled = 5;
+int button_stateA = 0;
+int button_stateB = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(greenled, OUTPUT);
+pinMode(redled, OUTPUT);
+
+//setup buttons
+Serial.begin(9600);
+pinMode(buttonA, INPUT);
+pinMode(buttonB, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  button_stateA = digitalRead(buttonA);
+  button_stateB = digitalRead(buttonB);
+
+  if (button_stateA == LOW && button_stateB == LOW)
+    {
+      Serial.print("Waiting...");
+    }
+
+   else if (button_stateA == HIGH && button_stateB == LOW)
+    {
+      Serial.println("Vending Soda");
+      digitalWrite(greenled, HIGH);
+      delay(1000);
+      digitalWrite(greenled, LOW);
+    }
+
+    else if (button_stateA == LOW && button_stateB == HIGH)
+      {
+        Serial.println("Vending water");
+        digitalWrite(greenled, HIGH);
+        delay(1000);
+        digitalWrite(greenled, LOW);
+      }
+
+    else if (button_stateA == HIGH && button_stateB == HIGH)
+      {
+        Serial.println("Product not available");
+        digitalWrite(redled, HIGH);
+        delay(1000);
+        digitalWrite(redled, LOW); 
+      }
+}
